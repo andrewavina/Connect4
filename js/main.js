15e71a2def2a64173ad56c5dcca923226dd63b51
Andrew Avina <avinaandrew@gmail.com>
2017-09-05 18:47:21 -0700

    js- diagonal win combos array

--- js/main.js
+++ js/main.js
@@ -1,53 +1,54 @@
-console.log("Loaded")
+console.log("JS Loaded")
 
 //global variables
 var player_1 = {
     "mark_color" : "blue",
     "data_name" : "Player 1",
     "wins" : 0
-};
-var player_1_moves = [];
+}
+
+var player_1_moves = []
+
 var player_2 = {
     "mark_color" : "red",
     "data_name" : "Player 2",
     "wins" : 0
-};
-var player_2_moves = [];
-var num_of_players = 0;
-var winner = false;
-var moves = 0;
+}
 
-//DOM manipulation
-var $question = $("#question")
-var $buttons_row_1_child = $(".buttons-row :first-child")
-var $pick_color = $("#pick-color")
-var $red_btn = $("#red-btn")
-var $blue_btn = $("#blue-btn")
-var $row = $(".row")
-var $circles = $("circle")
-var $buttons_row = $(".buttons-row")
+var player_2_moves = []
 
-// game function
-$(document).ready(function() {
-    function two_players(){
-        $("#submit").click(function(){
-            console.log($("input.#player-1").val())//mine test
-            console.log($("input.#player-2").val())//mine test 
-            $question.remove();
-            $("input").css("display", "none")
-            $("submit").remove()
-            $pick_color.html(
-                "Hello, " + $("#player-1").val() + " and " + $("#player-2").val() + "! " + "</br></br>" +
-                $("#player-1").val() + " is <span style='color: #4189C7;'>Blue</span>, and " + $("#player-2").val() + " is <span style='color: #C73D47;'>Red</span>.</br>" +
-                $("#player-1").val() + ", go first!"
-                )
-                $pick_color.css("display", "block");
-                $pick_color.css("margin", "25px 0");
+var num_of_players = 2
 
-                num_of_players = 2;
+var winner = false
 
-                make_board ();
-        })
-    }
-});
+var moves = 0 // to check if there is a draw at the end of the game
+
+// All possible combos of diagonal wins
+var diagonal_win = [
+    [0, 8, 16, 24],
+    [1, 9, 17, 25],
+    [2, 10, 18, 26],
+    [3, 9, 15, 21],
+    [3, 11, 19, 27],
+    [4, 10, 16, 22],
+    [5, 11, 17, 23],
+    [6, 12, 18, 24],
+    [7, 15, 23, 31],
+    [8, 16, 24, 32],
+    [9, 17, 25, 33],
+    [10, 16, 22, 28],
+    [10, 18, 26, 34],
+    [11, 17, 23, 29],
+    [12, 18, 24, 30],
+    [13, 19, 25, 31],
+    [14, 22, 30, 38],
+    [15, 23, 31, 39],
+    [16, 24, 32, 40],
+    [17, 23, 29, 35],
+    [17, 25, 33, 41],
+    [18, 24, 30, 36],
+    [19, 25, 31, 37],
+    [20, 26, 32, 38]
+];
+
 
